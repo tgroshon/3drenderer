65b099eaf5bce9a67efd532714bafbb2dcd4544c
Tommy Groshong <tom@groshong.software>
2022-02-20 22:46:24 -0500

    Extract display fns to separate files

--- 3drenderer/Main.c
+++ 3drenderer/Main.c
@@ -3,69 +3,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "display.h"
 
-// Global statics;
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-SDL_Texture *color_buffer_texture = NULL;
 bool is_running = false;
-uint32_t *color_buffer = NULL;
-
-// Default values with can be overidden by
-int window_width = 800;
-int window_height = 600;
-
-bool initialize_window(void) {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    fprintf(stderr, "Error initializing SDL.\n");
-    return false;
-  }
-
-  // query dimensions of full screen max width and height
-  // "fake fullscreen"
-  SDL_DisplayMode display_mode;
-  if (SDL_GetCurrentDisplayMode(0, &display_mode) >= 0) {
-    window_width = display_mode.w;
-    window_height = display_mode.h;
-  }
-
-  // Create and set the global static window
-  window = SDL_CreateWindow(NULL, // No title because going borderless
-                            SDL_WINDOWPOS_CENTERED, // position x
-                            SDL_WINDOWPOS_CENTERED, // position y
-                            window_width,           // width
-                            window_height,          // height
-                            SDL_WINDOW_BORDERLESS); // additional flags
-
-  if (!window) {
-    fprintf(stderr, "Error creating the SDL window.\n");
-    return false;
-  }
-
-  renderer = SDL_CreateRenderer(
-      window, // window object
-      -1,     // display device code; -1 for default visual device
-      0       // additional flags
-  );
-
-  if (!renderer) {
-    fprintf(stderr, "Error creating the SDL renderer.\n");
-    return false;
-  }
-
-  // "real fullscreen"
-  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-  return true;
-}
-
-void render_color_buffer() {
-  SDL_UpdateTexture(color_buffer_texture, // texture
-                    NULL,         // rectangle for getting subset of buffer
-                    color_buffer, // our color buffer
-                    (int)(window_width * sizeof(uint32_t))); // size of each row
-  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
 
 void setup() {
   // allocate memory of color buffer to fill one 32-bit number for every pixel
@@ -85,29 +25,6 @@ void setup() {
       SDL_TEXTUREACCESS_STREAMING, // continuous updating because we update each
                                    // frame
       window_width, window_height);
-}
-
-void safe_set_color_buffer(int address, uint32_t color) {
-  int max_address = window_width * window_height;
-  if (address > max_address) {
-    return;
-  }
-  color_buffer[address] = color;
-}
-
-/**
- * Tear-down everything we created
- * NOTE: kill in reverse order of initialization
- */
-void destroy_window() {
-  // NOTE: avoid freeing a NULL pointer
-  if (color_buffer != NULL) {
-    free(color_buffer);
-  }
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
 }
 
 void process_input() {
@@ -130,52 +47,6 @@ void update() {
   // TODO: implement
 }
 
-/**
- * @brief Sets every pixel of the color buffer to a new color
- */
-void clear_color_buffer(uint32_t new_color) {
-  for (int row = 0; row < window_height; row++) {
-    for (int column = 0; column < window_width; column++) {
-      safe_set_color_buffer((window_width * row) + column, new_color);
-    }
-  }
-}
-
-/**
- * @brief Example drawing a grid across the screen
- */
-void draw_grid() {
-  for (int y = 0; y < window_height; y++) {
-    for (int x = 0; x < window_width; x++) {
-      if (x % 20 == 0 || y % 20 == 0) {
-        safe_set_color_buffer((window_width * y) + x, 0xFF333333);
-      }
-    }
-  }
-}
-
-/**
- * @brief Example drawing a dotted grid across the screen
- */
-void draw_dotted_grid() {
-  for (int y = 0; y < window_height; y += 20) {
-    for (int x = 0; x < window_width; x += 20) {
-      safe_set_color_buffer((window_width * y) + x, 0xFF333333);
-    }
-  }
-}
-
-/**
- * @brief Example of drawing a rectangle on the screen
- */
-void draw_rect(int x, int y, int width, int height, uint32_t color) {
-  int starting_position = (window_width * y) + x;
-  for (int w = 0; w < width; w++) {
-    for (int h = 0; h < height; h++) {
-      safe_set_color_buffer(starting_position + (window_width * h) + w, color);
-    }
-  }
-}
 
 void render() {
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
@@ -190,6 +61,7 @@ void render() {
 
   SDL_RenderPresent(renderer);
 }
+
 
 int main(int argc, char *args[]) {
   printf("Program starting...\n");
---
+++ 3drenderer/display.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+// "extern" because declared here but not defined here
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+extern SDL_Texture *color_buffer_texture;
+extern uint32_t *color_buffer;
+extern int window_width;
+extern int window_height;
+
+
+bool initialize_window(void);
+void destroy_window(void);
+void render_color_buffer(void);
+void safe_set_color_buffer(int address, uint32_t color);
+void clear_color_buffer(uint32_t new_color);
+void draw_grid(void);
+void draw_dotted_grid(void);
+void draw_rect(int x, int y, int width, int height, uint32_t color);
