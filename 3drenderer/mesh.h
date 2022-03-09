c3474a0c97f605a60fd15705c39d46fbe5849d17
Tommy Groshong <tom@groshong.software>
2022-03-08 22:50:10 -0500

    Move face_t decl to mesh header
    It really didn't belong in triangle

--- 3drenderer/mesh.h
+++ 3drenderer/mesh.h
@@ -1,10 +1,27 @@
 #pragma once
+#include <stdint.h>
 #include "vector.h"
-#include "triangle.h"
 
-/// <summary>
-/// Defines a struct for dynamically sized meshes containing vertices, faces, and rotation
-/// </summary>
+
+/**
+ * Face represents the indexes of 3 points in a corresponding mesh point array.
+ *
+ * @details The ordering of which point you place in which field is significant
+ * because a clockwise ordering of points determines the forward side (normal)
+ * of the face; counter-clockwise denotes the back of the face.
+ *
+ * @note each field is base 1 instead of base 0
+ */
+typedef struct {
+  int a;
+  int b;
+  int c;
+  uint32_t color;
+} face_t;
+
+/**
+* Defines a struct for dynamically sized meshes containing vertices, faces, and rotation
+*/
 typedef struct {
   vec3_t *vertices; // dynamic array
   face_t *faces;    // dynamic array
--- 3drenderer/triangle.h
+++ 3drenderer/triangle.h
@@ -2,22 +2,6 @@
 #include <stdint.h>
 #include "vector.h"
 
-/**
- * Face represents the indexes of 3 points in a corresponding mesh point array.
- *
- * @details The ordering of which point you place in which field is significant
- * because a clockwise ordering of points determines the forward side (normal)
- * of the face; counter-clockwise denotes the back of the face.
- *
- * @note each field is base 1 instead of base 0
- */
-typedef struct {
-  int a;
-  int b;
-  int c;
-  uint32_t color;
-} face_t;
-
 typedef struct {
   vec2_t points[3];
   uint32_t color;
