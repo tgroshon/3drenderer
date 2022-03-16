81838ef69816e1f9b26d6376d21ddc058091c7c5
Tommy Groshong <tom@groshong.software>
2022-03-15 23:40:35 -0400

    Swap triangle texcoords with points when sorting

--- 3drenderer/triangle.c
+++ 3drenderer/triangle.c
@@ -1,8 +1,9 @@
-#include <stdint.h>
 #include "triangle.h"
 #include "vector.h"
+#include <stdint.h>
 
-triangle_t create_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color, float avg_depth){
+triangle_t create_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color,
+                           float avg_depth) {
   triangle_t tri;
   tri.color = color;
   tri.avg_depth = avg_depth;
@@ -23,42 +24,76 @@ void tri_int_swap(int *a, int *b) {
   *a = *b;
   *b = tmp;
 }
+void tri_float_swap(float *a, flaot *b) {
+  float tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
 
 triangle_t sort_tri_points_top_down(triangle_t tri) {
   int x0 = tri.points[0].x;
   int y0 = tri.points[0].y;
+  float u0 = tri.texcoords[0].u;
+  float v0 = tri.texcoords[0].v;
   int x1 = tri.points[1].x;
   int y1 = tri.points[1].y;
+  float u1 = tri.texcoords[1].u;
+  float v1 = tri.texcoords[1].v;
   int x2 = tri.points[2].x;
   int y2 = tri.points[2].y;
+  float u2 = tri.texcoords[2].u;
+  float v2 = tri.texcoords[2].v;
 
   // Step 1: sort points into flat-top+flat-bottom order: xy0 (top), xy1 (middle), xy2
   if (y0 > y1) {
     tri_int_swap(&y0, &y1);
     tri_int_swap(&x0, &x1);
+    tri_float_swap(&u0, &u1);
+    tri_float_swap(&v0, &v1);
   }
   if (y1 > y2) {
     tri_int_swap(&y1, &y2);
     tri_int_swap(&x1, &x2);
+    tri_float_swap(&u1, &u2);
+    tri_float_swap(&v1, &v2);
   }
   if (y0 > y1) {
     tri_int_swap(&y0, &y1);
     tri_int_swap(&x0, &x1);
+    tri_float_swap(&u0, &u1);
+    tri_float_swap(&v0, &v1);
   }
   triangle_t tri_result;
 
   vec2_t point0 = {.x = x0, .y = y0};
   vec2_t point1 = {.x = x1, .y = y1};
   vec2_t point2 = {.x = x2, .y = y2};
+  tex2_t texcoord0 = {
+      .u = u0,
+      .v = v0,
+  };
+  tex2_t texcoord1 = {
+      .u = u1,
+      .v = v1,
+  };
+  tex2_t texcoord2 = {
+      .u = u2,
+      .v = v2,
+  };
+
   tri_result.points[0] = point0;
   tri_result.points[1] = point1;
   tri_result.points[2] = point2;
+  tri_result.texcoords[0] = texcoord0;
+  tri_result.texcoords[1] = texcoord1;
+  tri_result.texcoords[2] = texcoord2;
 
   return tri_result;
 }
 
 vec2_t find_triangle_midpoint(triangle_t tri) {
-  // NOTE: triangle must have points already sorted down into flat-top+flat-bottom order: xy0 (top), xy1 (middle), xy2 (bottom)
+  // NOTE: triangle must have points already sorted down into flat-top+flat-bottom order:
+  // xy0 (top), xy1 (middle), xy2 (bottom)
 
   int x0 = tri.points[0].x;
   int y0 = tri.points[0].y;
